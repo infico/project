a58e428fe0b92bcc3c6635657bccebb933215e00
infico <praemix@protonmail.ch>
2019-06-20 11:23:14 +0200

    push with good js

--- map.js
+++ map.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var StorageTileLayer = L.TileLayer.extend({
+const StorageTileLayer = L.TileLayer.extend({
     _setUpTile: function (done, tile, value, blob) {
         tile.onload = L.bind(this._tileOnLoad, this, done, tile);
         tile.onerror = L.bind(this._tileOnError, this, done, tile);
@@ -9,23 +9,20 @@ var StorageTileLayer = L.TileLayer.extend({
     },
 
     createTile: function (coords, done) {
-        var tile = document.createElement('img');
+        const tile = document.createElement('img');
 
         if (this.options.crossOrigin) {
             tile.crossOrigin = '';
         }
 
-        /*
-         Alt tag is set to empty string to keep screen readers from reading URL and for compliance reasons
-         http://www.w3.org/TR/WCAG20-TECHS/H67
-        */
         tile.alt = '';
 
-        var x = coords.x,
-            y = this.options.tms ? this._globalTileRange.max.y - coords.y : coords.y,
-            z = this._getZoomForUrl(),
-            key = z + ',' + x + ',' + y,
-            self = this;
+        const x = coords.x,
+              y = this.options.tms ? this._globalTileRange.max.y - coords.y : coords.y,
+              z = this._getZoomForUrl(),
+              key = z + ',' + x + ',' + y,
+              self = this;
+
         if (this.options.storage) {
             this.options.storage.get(key, function (err, value) {
                 if (value) {
@@ -43,9 +40,9 @@ var StorageTileLayer = L.TileLayer.extend({
     }
 });
 
-var Control = L.Control.extend({
+const Control = L.Control.extend({
     onAdd: function (map) {
-        var container = L.DomUtil.create('div', 'leaflet-bar leaflet-control');
+        const container = L.DomUtil.create('div', 'leaflet-bar leaflet-control');
         container.innerHTML = '<a href="#" class="leaflet-control-zoom-in">' + this.options.innerHTML + '</a>';
         L.DomEvent
             .on(container, 'click', L.DomEvent.stopPropagation)
@@ -56,8 +53,8 @@ var Control = L.Control.extend({
     }
 });
 
-var ajax = function (src, responseType, callback) {
-    var xhr = new XMLHttpRequest();
+const ajax = function (src, responseType, callback) {
+    const xhr = new XMLHttpRequest();
     xhr.open('GET', src, true);
     xhr.responseType = responseType || 'text';
     xhr.onload = function(err) {
@@ -68,19 +65,19 @@ var ajax = function (src, responseType, callback) {
     xhr.send();
 };
 
-var dbname = 'tile';
-var db = new PouchDB(dbname);
-var map = L.map('map').setView([50.7167, 2.35], 13);
+const dbname = 'tile';
+const db = new PouchDB(dbname);
+let map = L.map('map').setView([50.7167, 2.35], 13);
 new StorageTileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {storage: db}).addTo(map);
 
 map.addControl(new Control({position: 'topleft', innerHTML: 'C', handler: function () {
     ajax('cache_keys.json', 'text', function (response) {
-        var tile_key_list = JSON.parse(response);
-        for (var i = 0, l = tile_key_list.length; i < l; i++) {
+        const tile_key_list = JSON.parse(response);
+        for (let i = 0, l = tile_key_list.length; i < l; i++) {
             (function (key) {
-                var src = 'http://tile.osm.org/' + key.split(',').join('/') + '.png';
+                const src = 'http://tile.osm.org/' + key.split(',').join('/') + '.png';
                 ajax(src, 'blob', function (response) {
-                    var reader = new FileReader();
+                    const reader = new FileReader();
                     reader.onloadend = function(e) {
                         db.put({_id: key, v: e.target.result});
                     };
@@ -93,7 +90,7 @@ map.addControl(new Control({position: 'topleft', innerHTML: 'C', handler: functi
 
 document.getElementById('buttonbar').onclick = () => {
     document.getElementById('map').innerHTML = "<div id='map' style='width: 100%; height: 100%;'></div>";
-    var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    const osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
         osmAttribution = 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors,' +
             ' <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
         osmLayer = new L.TileLayer(osmUrl, {maxZoom: 18, attribution: osmAttribution});
@@ -109,7 +106,7 @@ document.getElementById('buttonbar').onclick = () => {
             const longitude = data.results[0].geometry.lng;
             map.setView(new L.LatLng(latitude, longitude), 15);
             map.addLayer(osmLayer);
-            var validatorsLayer = new OsmJs.Weather.LeafletLayer({lang: 'en'});
+            const validatorsLayer = new OsmJs.Weather.LeafletLayer({lang: 'en'});
             map.addLayer(validatorsLayer);
         });
 };
